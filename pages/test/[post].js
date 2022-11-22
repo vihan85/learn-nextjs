312b371c99867214c36f838e4059ca059c6004f6
vihan85 <hanthuyvi0805@gmail.com>
2022-11-22 19:17:25 +0700

    file system routing

---
+++ pages/test/[post].js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+function Post() {
+    return (
+        <Link href={'/ass'}>
+            <h1>Post</h1>
+        </Link>
+    );
+}
+export default Post;
---
+++ pages/test/folow.js
@@ -0,0 +1,4 @@
+function Folow() {
+    return <h1>Folow</h1>;
+}
+export default Folow;
